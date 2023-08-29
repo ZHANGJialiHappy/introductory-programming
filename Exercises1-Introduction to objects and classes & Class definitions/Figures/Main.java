fa3646475e4e1bb293004c2f6bf2d79212dadee8
Jiali Zhang <zhangjiali86@gmail.com>
2023-08-29 21:22:20 +0200

    object and class excercise

---
+++ Exercises1-Introduction to objects and classes & Class definitions/Figures/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // provided code
+        Triangle triangle = new Triangle(); // Example of how to create an object.
+        triangle.makeVisible(); // Makes you able to see it when you run the code.
+        // Canvas.getCanvas(); // Creates the picture you can see it on.
+
+        // Exercise 1.1
+        // triangle.makeInvisible();
+        // Circle cirle = new Circle();
+        // cirle.makeVisible();
+        // Square square = new Square();
+        // square.makeVisible();
+
+        // Exercise 1.2
+
+        // Exercise 1.3
+
+        // Exercise 1.4
+
+        // Exercise 1.6
+    }
+}
