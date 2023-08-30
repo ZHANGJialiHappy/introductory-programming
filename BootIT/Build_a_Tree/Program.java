00397508ee82397bea2657ce76e35a4bfbfe95c0
Jiali Zhang <zhangjiali86@gmail.com>
2023-08-30 14:02:42 +0200

    add BootIT

---
+++ BootIT/Build_a_Tree/Program.java
@@ -0,0 +1,17 @@
+package Build_a_Tree;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(".........");
+        Tree Jiali = new Tree(1, 1, 1, 11);
+        Jiali.display();
+        System.out.println(".........");
+        Jiali.grow();
+        Jiali.grow();
+        Jiali.grow();
+        Jiali.grow();
+        System.out.println(".........");
+        Jiali.display();
+    }
+
+}
