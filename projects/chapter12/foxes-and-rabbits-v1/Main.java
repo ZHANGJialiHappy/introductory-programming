fc819f6162edb5574f53326ee1b28dc5b0f6f89d
Jiali Zhang <zhangjiali86@gmail.com>
2023-10-09 14:26:39 +0200

    update

---
+++ projects/chapter12/foxes-and-rabbits-v1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Simulator a = new Simulator(50, 50);
+        a.simulateOneStep();
+        // a.simulateOneStep();
+        // a.simulateOneStep();
+        a.runLongSimulation();
+
+    }
+
+}
