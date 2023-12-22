86424761c4b299b394d4a22b832bad2dfba49d66
Jiali Zhang <zhangjiali86@gmail.com>
2023-12-22 14:26:49 +0100

    update

--- Exercise6-Inheritance/10 testing source code/app/src/main/java/Testing/Examples/AssertionExamples.java
+++ Exercise6-Inheritance/10 testing source code/app/src/main/java/Testing/Examples/AssertionExamples.java
@@ -2,18 +2,14 @@ package Testing.Examples;
 
 public class AssertionExamples {
 
-    public boolean isEven(int num){
+    public boolean isEven(int num) {
         return (num % 2 == 0);
-    
+
     }
 
-    public String nameConcat(String firstName, String lastName){
-        return firstName+" "+lastName;
-        
+    public String nameConcat(String firstName, String lastName) {
+        return firstName + " " + lastName;
+
     }
-    
+
 }
- 
-
-
-
--- Exercise6-Inheritance/10 testing source code/app/src/test/java/Testing/Examples/AssetionExamplesTest.java
+++ Exercise6-Inheritance/10 testing source code/app/src/test/java/Testing/Examples/AssetionExamplesTest.java
@@ -5,66 +5,36 @@ import org.junit.jupiter.api.TestInstance;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class AssetionExamplesTest {
 
     private AssertionExamples systemUnderTest;
 
-    @BeforeAll 
-    void init(){
-        systemUnderTest=new AssertionExamples();
+    @BeforeAll
+    void init() {
+        systemUnderTest = new AssertionExamples();
     }
 
     @Test
-    void isEven_evenNumber_returnsTrue(){
-        assertEquals(true,systemUnderTest.isEven(100));
+    void isEven_evenNumber_returnsTrue() {
+        assertEquals(true, systemUnderTest.isEven(100));
     }
 
     @Test
-    void isEven_evenNumber_returnsTrue2(){
+    void isEven_evenNumber_returnsTrue2() {
         assertTrue(systemUnderTest.isEven(22));
     }
 
     @Test
-    void isEven_oddNumber_returnsFalse(){
+    void isEven_oddNumber_returnsFalse() {
         assertFalse(systemUnderTest.isEven(55));
     }
 
-
     @Test
-    void nameConcat_nonNullValues_returnsNotNull(){
+    void nameConcat_nonNullValues_returnsNotNull() {
         assertNotNull(systemUnderTest.nameConcat("Alice", "Johnsson"));
-        
+
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
